02f4ee00ecc55e1288096c07139540d96264371b
Marek Vojtek <voj39210@spseol.cz>
2023-03-21 14:07:24 +0100

    ještě se bude dodělávat

--- grafy tabulkove.py
+++ grafy tabulkove.py
@@ -9,13 +9,33 @@ from tkinter import filedialog, messagebox
 import matplotlib.pyplot as plt
 import pylab as pl
 
+
+class MyEntry(tk.Entry):
+    def __init__(self, master=None, cnf={}, **kw):
+        super().__init__(master, cnf, **kw)
+
+        if not "textvariable" in kw:
+            self.variable = tk.StringVar()
+            self.config(textvariable=self.variable)
+        else:
+            self.variable = kw["textvariable"]
+
+    @property
+    def value(self):
+        return self.variable.get()
+
+    @value.setter
+    def value(self, new: str):
+        self.variable.set(new)
+
+
 def close_window():
     window.destroy()
 
 def choose_file():
     filename = filedialog.askopenfilename()
     if filename:
-        chosen_file_label.config(text="Vybraný soubor: " + filename)
+        chosen_file_label.config(text=filename)
         show_button.config(state="normal", command=lambda: show_values(filename))
     else:
         chosen_file_label.config(text="")
@@ -38,19 +58,26 @@ def show_values(filename):
 
     elif data_dir.get() == 'SLOUP':
         with open(filename, "r") as file:
-            for line in file:
-                x, y = line.split()
-                x = x + (float(x))
-                y = y + (float(y))
+            x = []
+            y = []
+            while True:
+                line = file.readline()
+                if line =='':
+                    break
+                if ';' not in line:
+                    continue
+                x1,y1 = line.split(';')
+                x.append(float(x1.replace(',','.')))
+                y.append(float(y1.replace(',','.')))
     print(x)
-    plt.plot(x,y, 'b.-')
-    plt.title("Graph")
-    plt.xlabel("X Values")
-    plt.ylabel("Y Values")
+    plt.plot(x,y, linestyle = lineVar.get(),marker = marker.get())
+    plt.title(titleEntry.value)
+    plt.xlabel(xlabelEntry.value)
+    plt.ylabel(ylabelEntry.value)
     plt.show()
 
 window = tk.Tk()
-window.geometry("450x100+100+50")
+window.geometry("570x250")
 
 window.title("Grafy")
 
@@ -70,13 +97,37 @@ quit_button = tk.Button(button_frame, text="Quit", command=close_window)
 quit_button.pack(side="left")
 
 sloup_frame = tk.Frame(button_frame)
-sloup_frame.pack(anchor='s')
+sloup_frame.pack(anchor='e')
 
 data_dir = tk.StringVar()
 button_rad = tk.Radiobutton(sloup_frame, text = 'Data jsou v řádních',variable=data_dir,value='RAD')
 button_rad.pack(anchor='w')
 button_sloup = tk.Radiobutton(sloup_frame, text = 'Data jsou ve sloupcích',variable=data_dir,value='SLOUP')
-button_sloup.pack(anchor='e')
+button_sloup.pack(anchor='w')
+
+graf_frame = tk.LabelFrame(sloup_frame, text='GRAF')
+graf_frame.pack(padx = 5,pady = 5, fill = 'x', anchor='s')
+
+tk.Label(graf_frame, text='Titulek').grid(row=0,column=0)
+titleEntry = MyEntry(graf_frame)
+titleEntry.grid(row=0,column=1)
+
+tk.Label(graf_frame,text='osa X').grid(row = 1, column = 0)
+xlabelEntry = MyEntry(graf_frame)
+xlabelEntry.grid(row = 1, column = 1)
+
+tk.Label(graf_frame,text='osa Y').grid(row = 2, column = 0)
+ylabelEntry = MyEntry(graf_frame)
+ylabelEntry.grid(row = 2, column = 1)
+
+tk.Label(graf_frame,text='STYL ČÁRY').grid(row = 3, column = 0)
+lineVar = tk.StringVar(value='none')
+tk.OptionMenu(graf_frame,lineVar,'none','-','-.','--',':').grid(row=3,column=2, sticky='w')
+
+tk.Label(graf_frame,text='Marker').grid(row = 4, column = 0)
+marker = tk.StringVar(value='none')
+tk.OptionMenu(graf_frame,marker,*tuple('.,o+PxX*1234<>v')).grid(row=4,column=2, sticky='w')
+
 
 chosen_file_label = tk.Label(window, text="")
 chosen_file_label.pack()
